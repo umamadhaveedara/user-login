9ae21185784b54c9fd20129b93db1fcae2c3032c
UmaMadhav Eedara <umamadhaveedara@gmail.com>
2023-09-17 16:51:11 +0530

    dashbord updated

--- dashbord.js
+++ dashbord.js
@@ -12,8 +12,9 @@ const imagePreview = document.getElementById("image-preview");
 const photomgs = document.getElementById("photomgs");
 // const userBox = document.getElementById("userBox");
 const dataBox = document.getElementById("dataBox");
-
-let cardURL = ""
+const addUserMainBtn = document.getElementById("addUserMainBtn")
+let cardURL = "";
+let users = [];
 
 function profile() {
   window.open("profile.html", "_self");
@@ -27,11 +28,13 @@ function logout1() {
 
 function addUserBtn() {
   addUserPopUp.style.display = "block";
+  addUserMainBtn.innerText = "Add User"
 }
 function ClosePopUp(event) {
   addUserPopUp.style.display = "none";
   event.preventDefault();
 }
+
 var validfirstnamedata;
 var validlastnamedata;
 var validemaildata;
@@ -85,8 +88,8 @@ lastname.addEventListener("input", () => {
 imageInput.addEventListener("change", function () {
   const file = imageInput.files[0];
   if (file) {
-    const imageURL = URL.createObjectURL(file);
-    console.log(imageURL,"here")
+    var imageURL = URL.createObjectURL(file);
+    console.log(imageURL, "here");
     imagePreview.src = imageURL;
     cardURL = imageURL;
     validPhotoData = true;
@@ -98,6 +101,54 @@ imageInput.addEventListener("change", function () {
   }
 });
 
+function adduser() {
+  var firstname1 = firstname.value;
+  var lastname1 = lastname.value;
+  var email1 = email.value;
+  var phone1 = phone.value;
+  const user = { firstname1, lastname1, email1, phone1, image: cardURL };
+  users.push(user);
+  addUserPopUp.style.display = "none";
+}
+
+function displayuser() {
+  dataBox.innerHTML = "";
+  users.forEach((userdata, index) => {
+    const card = document.createElement("div");
+    card.className = "userBox";
+    card.innerHTML = `<div class="userIcons"><button onclick="updateUser(${index})"><i class="fa-solid fa-pen"></i></button><button onclick="deleteUser(${index})"><i class="fa-solid fa-trash"></i></button></div><div class="userMainInfo"><div class="userImg"><img src="${userdata.image}" alt="" srcset=""></div><div class="userdata"><p>Name: ${userdata.firstname1} ${userdata.lastname1}</p><p>Email: ${userdata.email1}</p><p>Mobile No: ${userdata.phone1}</p></div></div>`;
+    dataBox.appendChild(card);
+  });
+}
+
+function deleteUser(index) {
+  URL.revokeObjectURL(users[index].image);
+  users.splice(index, 1);
+  displayuser();
+}
+
+function updateUser(index) {
+  const user = users[index];
+  document.getElementById("firstname").value = user.firstname1;
+  document.getElementById("lastname").value = user.lastname1;
+  document.getElementById("email").value = user.email1;
+  document.getElementById("phone").value = user.phone1;
+  document.getElementById("image-input").value = "";
+  addUserPopUp.style.display = "block";
+  addUserMainBtn.innerText = "Update User"
+  deleteUser(index);
+}
+
+function clearForm() {
+  document.getElementById("firstname").value = "";
+  document.getElementById("lastname").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("phone").value = "";
+  document.getElementById("image-input").value = "";
+  imagePreview.src = "";
+}
+
+
 function valid(event) {
   if (
     validfirstnamedata &&
@@ -107,62 +158,46 @@ function valid(event) {
     validPhotoData
   ) {
     toastr.success("Sucessfully User Added");
-    console.log(imagePreview,"here2")
-    addUserPopUp.style.display = "none";
-    dataBox.innerHTML += `  <div class="userBox" id="userBox">
-    <div class="userIcons">
-      <i class="fa-solid fa-pen"></i>
-      <i class="fa-solid fa-trash"></i>
-    </div>
-    <div class="userMainInfo">
-      <div class="userImg"><img src="${cardURL}" alt="" srcset=""></div>
-      <div class="userdata">
-        <p>Name: ${firstname.value}</p>
-        <p>Email: ${email.value}</p>
-        <p>Mobile No: ${phone.value}</p>
-      </div>
-    </div>
-  </div>`
+    adduser();
+    displayuser();
+    clearForm();
     event.preventDefault();
-    firstname.value ="";
-    lastname.value ="";
-    email.value ="";
   } else if (validemaildata) {
     firstnamemgs.textContent = "This field is required";
     lastnamemgs.textContent = "This field is required";
     phonemgs.textContent = "This field is required";
     photomgs.textContent = "This field is required";
     event.preventDefault();
-  }else if (validfirstnamedata) {
+  } else if (validfirstnamedata) {
     emailmgs.textContent = "This field is required";
     lastnamemgs.textContent = "This field is required";
     phonemgs.textContent = "This field is required";
     photomgs.textContent = "This field is required";
     event.preventDefault();
-  }else if (validlastnamedata) {
+  } else if (validlastnamedata) {
     emailmgs.textContent = "This field is required";
     firstnamemgs.textContent = "This field is required";
     phonemgs.textContent = "This field is required";
     photomgs.textContent = "This field is required";
     event.preventDefault();
-  }else if (validPhoneData) {
+  } else if (validPhoneData) {
     emailmgs.textContent = "This field is required";
     firstnamemgs.textContent = "This field is required";
     lastnamemgs.textContent = "This field is required";
     photomgs.textContent = "This field is required";
     event.preventDefault();
-  }else if (validPhotoData) {
+  } else if (validPhotoData) {
     emailmgs.textContent = "This field is required";
     firstnamemgs.textContent = "This field is required";
     lastnamemgs.textContent = "This field is required";
     phonemgs.textContent = "This field is required";
     event.preventDefault();
-  }else{
+  } else {
     emailmgs.textContent = "This field is required";
     firstnamemgs.textContent = "This field is required";
     lastnamemgs.textContent = "This field is required";
     phonemgs.textContent = "This field is required";
     photomgs.textContent = "This field is required";
-    event.preventDefault()
+    event.preventDefault();
   }
 }
